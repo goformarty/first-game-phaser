9764bd2335db0d1e9b7519e9d901d5aa71ad2724
GoForMarty <marty.stepien@gmail.com>
2017-06-28 21:00:23 +0100

    stars, collecting start and jumping added

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ window.onload = function() {
   var player;
   var platforms;
   var cursors;
+  var stars;
 
   function create() {
 
@@ -64,6 +65,25 @@ window.onload = function() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    //  Finally some stars to collect
+    stars = game.add.group();
+
+    //  We will enable physics for any star that is created in this group
+    stars.enableBody = true;
+
+    //  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 12; i++)
+    {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
+
+        //  Let gravity do its thing
+        star.body.gravity.y = 300;
+
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+
     cursors = game.input.keyboard.createCursorKeys();
 
 
@@ -71,39 +91,51 @@ window.onload = function() {
 
   function update() {
 
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
+      //  Collide the player and the stars with the platforms
+      game.physics.arcade.collide(player, platforms);
+      game.physics.arcade.collide(stars, platforms);
 
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
+      //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+      game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
-    if (cursors.left.isDown)
-    {
-      //  Move to the left
-      player.body.velocity.x = -150;
+      //  Reset the players velocity (movement)
+      player.body.velocity.x = 0;
 
-      player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-      //  Move to the right
-      player.body.velocity.x = 150;
+      if (cursors.left.isDown)
+      {
+          //  Move to the left
+          player.body.velocity.x = -150;
 
-      player.animations.play('right');
-    }
-    else
-    {
-      //  Stand still
-      player.animations.stop();
+          player.animations.play('left');
+      }
+      else if (cursors.right.isDown)
+      {
+          //  Move to the right
+          player.body.velocity.x = 150;
 
-      player.frame = 4;
-    }
+          player.animations.play('right');
+      }
+      else
+      {
+          //  Stand still
+          player.animations.stop();
 
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
-      player.body.velocity.y = -350;
-    }
+          player.frame = 4;
+      }
+
+      //  Allow the player to jump if they are touching the ground.
+      if (cursors.up.isDown && player.body.touching.down)
+      {
+          player.body.velocity.y = -350;
+      }
+
+  }
+
+  function collectStar (player, star) {
+
+      // Removes the star from the screen
+      star.kill();
+
   }
 
 };
